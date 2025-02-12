db126f53009efe91a709395628a5536ede5503ae
oumarKingDev <oumarciss300@gmail.com>
2025-02-12 16:23:58 +0100

    adding word to the list

--- azerType/scripts/config.js
+++ azerType/scripts/config.js
@@ -1,6 +1,6 @@
 // Ceci est un fichier de configuration qui contiendrait nos deux tableaux listeMots et listePhrases. Cela nous permettrait de faire évoluer ces tableaux sans risquer de modifier par erreur la logique du code ! 😃
 // Listes de mots et phrases
-let listeMots = ['cachalot', 'voiture', 'maison', 'tour', 'paix', 'joix', 'meilleure']
+let listeMots = ['cachalot', 'voiture', 'maison', 'tour', 'paix', 'joix', 'meilleure','king']
 let listePhrases = ["La vie, l'univers et le reste", "Merci pour  le poisson", "Pas de panique"] 
 
 // Comme ils ne sont pas dans un bloc de code alors ils sont globals en JS
