37ea778c82edf721c5bc7a0dec359d3edf48fcd6
oumarKingDev <oumarciss300@gmail.com>
2025-02-12 19:50:33 +0100

    well Done the Project is over!!!

--- azerType/scripts/script.js
+++ azerType/scripts/script.js
@@ -2,21 +2,10 @@ console.log("Hello, world");
 
 
 let inputSaisie = document.getElementById("inputSaisie")
-// console.log(inputSaisie)
-
-// console.log(btnValidation)
 let motPropose = document.querySelector(".zoneProposition")
-// console.log(motPropose)
 let zoneScore = document.querySelector("#score span")
-// console.log(zoneScore)
-let btnRadio = document.querySelectorAll(".zoneChoix input")
-// console.log(btnRadio)
 let inputMot = document.getElementById("mots")
-// console.log(inputMot)
 let inputPhrases = document.getElementById("phrases")
-// console.log(inputPhrases)
-let valueRadiobtn = btnRadio[0].getAttribute("value")
-// console.log(value)
 let listRadioBtn = document.querySelectorAll('input[type="radio"]')
 
 
@@ -31,28 +20,11 @@ function lancerJeu(){
     let nbMotsProposez = 0 ;
     //1. Proposez un mot l'utilisateur
 
-    let choix = choisirPhrasesOuMots() 
+
     let liste = listeMots
 
-
-
-    //2. verifier si le mot saisie est correct ou non
-    if(choix === "mots"){
-        // S'il choisit "mots"
-        // score = lancerBoucleDeJeu(listeMots)
-        liste = [...listeMots]
-        nbMotsProposez = listeMots.length
-        
-
-    }else{
-        // S'il choisit phrase
-        // score = lancerBoucleDeJeu(listePhrases)
-        liste = [...listePhrases]
-        nbMotsProposez = listePhrases.length
-    }
-    
-    console.log(listRadioBtn)
-    for(let index =0;index<listRadioBtn.length;index++){
+    motPropose.innerHTML = listeMots[0]
+    for(let index =0; index <  listRadioBtn.length;index++){
         listRadioBtn[index].addEventListener("change", (event) =>{
             console.log(event.target.value)
             if(event.target.value === "1"){
@@ -67,7 +39,6 @@ function lancerJeu(){
         })
     }
 
-    motPropose.innerHTML = liste[0]
     
     let btnValidation = document.getElementById("btnValidation")
 
@@ -115,44 +86,9 @@ function afficherResultat(score, nombreQuestion){
     zoneScore.innerHTML = `${score} / ${nombreQuestion}`;  // mettre a jour le resultat YESSSSSSSSSSSSSSSSSSS!!!!!!!!!!!
 }
 
-function choisirPhrasesOuMots(){
-    // let choix = prompt("mots ou phrase: ")
-    let radioBtnValue = "";
-    for(let i=0; i<listRadioBtn.length;i++){
-        if(listRadioBtn[i].checked){
-            radioBtnValue = listRadioBtn[i].value
-            
-        }
-    }
-    console.log(radioBtnValue)
-    if(radioBtnValue === "1"){
-        choix = "mots"
-    }else{
-        choix = "phrases"
-    }
-
-    return choix
-}
 
 
 
-// function lancerBoucleDeJeu(listeMotsOuPhrase){
-//     let score = 0
 
-//     for(let i=0;i< listeMotsOuPhrase.length;i++){
-//         let motOuPhrase = prompt("Saisir un mot: "+ listeMotsOuPhrase[i])
-//         // // Proposer les mots un a un 
-//         // motPropose.innerHTML = `${listeMotsOuPhrase[i]}`
-//         // let motOuPhrase = inputSaisie.value
-//         // console.log(motOuPhrase)
-//         // Ajout un addEvenListener  au btn Vlaider
 
-//         if(motOuPhrase === listeMotsOuPhrase[i]){
-//             console.log("Correct vous avez tapez le bon mot ou phrase")
-//             score++
-//         }
-//     }
-
-//     return score
-// }
 
